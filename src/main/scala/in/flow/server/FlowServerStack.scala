3eb12bec88de31985b633da2a09ae045b43077d4
anton <antonkats@gmail.com>
2017-04-02 20:40:55 -0400

    server no longer exists on return

--- src/main/scala/in/flow/server/FlowServerStack.scala
+++ src/main/scala/in/flow/server/FlowServerStack.scala
@@ -77,9 +77,9 @@ object FlowServerStack extends InnerRoutes {
     val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
     println("Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+//    StdIn.readLine() // let it run until user presses return
+//    bindingFuture
+//      .flatMap(_.unbind()) // trigger unbinding from the port
+//      .onComplete(_ => system.terminate()) // and shutdown when done
   }
 }
