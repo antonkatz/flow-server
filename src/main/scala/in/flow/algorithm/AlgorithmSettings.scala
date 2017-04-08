453b47eec8139bf72893840964a3f09235e6392a
anton <antonkats@gmail.com>
2017-04-08 08:35:03 -0400

    changing model

--- src/main/scala/in/flow/algorithm/AlgorithmSettings.scala
+++ src/main/scala/in/flow/algorithm/AlgorithmSettings.scala
@@ -2,13 +2,13 @@ package in.flow.algorithm
 
 object AlgorithmSettings {
   /** How deep to search for connections, eg friends only, friends of friends, friends of friends of friends */
-  val connections_search_depth = 3 //friends of friends of friends
+  val connections_search_depth = 2 //friends of friends
 
   /** The smallest possible amount for a transaction, in hours (6 min) */
   val transaction_amount_lower_bound: BigDecimal = 0.1
 
   /** in what time the financial principal doubles, in seconds */
-  val principle_double_in: Long = 60 * 60 * 24 * 7 * 2 // two weeks
+  val principle_double_in: Long = 60 * 60 * 24 * 30 // 30 days
 //  val principle_double_in: Long = 60 * 60
 
   /** how often to commit uncommitted interest, in millis */
